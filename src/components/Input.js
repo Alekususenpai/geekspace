09713fb75d7c96b04a4eccd0b6a71ddd6444abae
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-08-15 23:54:51 +0200

    input comp

---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Input() {
+          return (
+                    <div>
+                              <label for=""></label>
+                              <input type="text" id=""></input>
+                    </div>
+          )
+}
+
+export default Input
